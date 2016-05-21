84eac672662bf6c823682cb7cbdd2ff656ea57e3
Andrea Biancini <andrea.biancini@gmail.com>
2016-05-21 11:00:33 +0200

    First test of integration

--- views/main/mainCtrl.js
+++ views/main/mainCtrl.js
@@ -13,6 +13,24 @@ define([
 	app.controller('views/MainCtrl', function ($scope) {
 		$scope.app = qlik.openApp('SiWeb Risorse.qvf', config);
 		
+		$scope.assunti_currentmonth = -1;
+		$scope.app.createCube({
+			qDimensions: [ ],
+			qMeasures: [{
+				qDef: { qDef: 'count({<GenericDate={"$(=max(GenericDate))"}, FLG_DISPONIBILE={1}, _Active={1}>} DISTINCT ID_RISORSA)' }
+			}],
+			qInitialDataFetch: [{
+				qHeight: 1000,
+				qWidth: 5
+			}]
+		}, function (reply) {
+			$.each(reply.qHyperCube.qDataPages[0].qMatrix, function (index, value) {
+				if (value[0] != null && value[0].qNum != "NaN") {
+					$scope.assunti_currentmonth = value[0].qNum;
+				}
+			});
+		});
+		
 		$(document).ready(function() {
 			// Line chart from swirlData for dashReport
 			var ctx = document.getElementById("dashReport").getContext("2d");
